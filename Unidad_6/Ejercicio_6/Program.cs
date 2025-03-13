3bb0c64dd75aee7b88571f3affc6f9876e9bc098
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-13 20:08:57 -0300

    Ejercicio 6 | Mayor de 5 numeros

---
+++ Unidad_6/Ejercicio_6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       /*
+        Crea un programa que pida ingresar 5 números, uno por uno, y determine cuál es el mayor número ingresado.
+       */
+
+        int numero, numeroMayor = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > numeroMayor)
+            {
+                numeroMayor = numero;
+            }
+        }
+
+        Console.WriteLine("El numero mas grande es: " + numeroMayor);
+
+    }
+}
