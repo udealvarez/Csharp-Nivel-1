8fe32df9af0ac438701fad83f2f50bd0c7bd87ad
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-13 21:39:29 -0300

    Ejercicio 8 | Ser o no ser primo

---
+++ Unidad_6/Ejercicio_8/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            Escribe un programa que determine si un número ingresado por el usuario es un número primo o no. 
+            Un número es primo si es divisible solo por 1 y por sí mismo.
+        */
+
+        int numero = 0, contador = 0;
+
+        Console.WriteLine("Ingrese un numero: ");
+        numero = int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                contador++;
+            }
+        }
+
+        if (contador == 2)
+        {
+            Console.WriteLine("Es primo");
+        }else
+        {
+            Console.WriteLine("No es primo");            
+        }
+    }
+}
