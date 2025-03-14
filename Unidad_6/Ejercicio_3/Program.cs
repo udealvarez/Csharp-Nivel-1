3a44ba367b79ce137d1fcaa48ce99b248efd0481
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-13 22:15:53 -0300

    Ejercicio 3

---
+++ Unidad_6/Ejercicio_3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ejercicio_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            Hacer un programa para recibir listas de números positivos que están separadas entre sí por un cero. 
+            El fin de la carga se notifica con un número negativo. Luego mostrar cuántos números tiene cada lista.
+        */
+
+        int numero, contador = 0, numeroLista = 1;
+
+        Console.WriteLine("Ingrese numeros positivos, separados por un 0. Finaliza con un numero negativo: ");
+        numero = int.Parse(Console.ReadLine());
+
+        while (numero >= 0)
+        {
+            if (numero == 0)
+            {
+                Console.WriteLine("La lista " + numeroLista + " tiene " + contador + " numeros");
+                numeroLista++;
+                contador = 0;
+            }else
+            {
+                contador++;                
+            }
+
+            numero = int.Parse(Console.ReadLine());
+        }
+    }
+}
