38e5e8ed018742f329d6541228e9892758cc1abe
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-18 14:23:06 -0300

    Ejercicio 3

--- Unidad_7/Ejercicio_3/Program.cs
+++ Unidad_7/Ejercicio_3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /*
+            Hacer un programa que solicite una cadena de caracteres y dos caracteres sueltos (tres ingresos). 
+            El programa deberá generar una cadena donde todas las ocurrencias del primer carácter dado hayan sido reemplazadas por el segundo. 
+            Mostrar el resultado en pantalla. Ejemplo:
+
+            CADENA FUENTE: “La mar estaba serena"
+            CARÁCTER 1: ‘a’ CARÁCTER 2: ‘i’
+            CADENA RESULTADO: “Li mir estibi sereni"
+        */
+
+        string cadena, nuevaCadena = "";
+        char caracterA, caracterB;
+
+        Console.WriteLine("Ingrese la cadena de caracteres: ");
+        cadena = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el primer caracter para reemplazar: ");
+        caracterA = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo caracter para ser reemplazado: ");
+        caracterB = char.Parse(Console.ReadLine());
+
+        for (int i = 0; i < cadena.Length; i++)
+        {
+            if (cadena[i] == caracterA)
+            {
+                nuevaCadena += caracterB;     // reemplazo de caracter
+            }else{
+                nuevaCadena += cadena[i];
+            }
+        }
+
+        Console.WriteLine("Resultado: " + nuevaCadena);
+
     }
 }
