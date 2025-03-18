8107427d873562786f8de6ba0b7c89439737259e
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-18 14:09:33 -0300

    Vectores de caracteres | Parte 2 | String

---
+++ Unidad_7/Ejemplo_Vectores5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ejemplo_Vectores5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // PARTE 2 | CADENA DE CARACTERES
+
+
+        string nombre;
+        Console.WriteLine("Ingrese su nombre..");
+        nombre = Console.ReadLine();
+        
+        Console.WriteLine("Hola " + nombre);
+
+        // Profesor: Maximiliano Oscar San Fernandez    
+
+    }
+}
