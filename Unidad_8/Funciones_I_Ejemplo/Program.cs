4a3db27311fc44cda4cbb9e0139d0a46eadff0be
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-19 21:31:24 -0300

    Ejemplo de funciones con nuestro primer programa | Calculadora

---
+++ Unidad_8/Funciones_I_Ejemplo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Funciones_I_Ejemplo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2, resultado;
+
+        Console.WriteLine("Ingrese un numero: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese un numero: ");
+        num2 = int.Parse(Console.ReadLine());
+        
+
+        resultado = sumar(num1, num2);      // llamo a la funcion y por parametros le paso los valores que pedi ingresar
+
+        Console.WriteLine("El resultado es: " + resultado);
+    }
+
+    static int sumar(int a, int b){
+
+        int r;
+        r = a + b;
+        return r;
+    }
+
+}
