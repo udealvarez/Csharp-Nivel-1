e79450f1e6d1de44ea883db8015b5629fada2127
Eduardo AlvareZ <udealvarez@icloud.com>
2025-03-20 15:09:10 -0300

    Ejercicio 4

---
+++ Unidad_8/Ejercicio_4/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ejercicio_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Hacer una función de tipo void (porque no va a devolver nada) llamada “positivoNegativoCero” que reciba un número por valor y una variable por referencia. 
+        Que analice el número y escriba variable recibida por referencia con:
+
+            a. 1 si el número es positivo.
+            b. -1 si el número es negativo.
+            c. 0 si el número es cero.
+        */
+
+        int numero, resultado = 0;
+
+        Console.WriteLine("Ingresar un numero: ");
+        numero = int.Parse(Console.ReadLine());
+
+        positivoNegativoCero(numero, ref resultado);
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+
+
+    static void positivoNegativoCero(int number, ref int R){
+
+        if (number > 0)
+        {
+            R = 1;
+        }else if (number < 0)
+        {
+            R = -1;
+        }else
+        {
+            R = 0;
+        }
+    }
+}
